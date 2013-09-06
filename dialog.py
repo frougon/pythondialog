26b736c43c4b8d55981be3f7084307467175d11b
Florent Rougon <f.rougon@free.fr>
2013-09-06 15:50:11 +0200

    Add version_info to dialog.py
    * dialog.py: new 'version_info' module-level attribute, similar to what
    the 'sys' module provides. This avoids the need to parse __version__
    when one wants to extract for instance the major and/or minor version
    number of pythondialog.

--- dialog.py
+++ dialog.py
@@ -75,7 +75,23 @@ replace "except PythonDialogIOError" clauses with
 
 """
 
-__version__ = "2.13.1"
+import collections
+_VersionInfo = collections.namedtuple(
+    "VersionInfo", ("major", "minor", "micro", "releasesuffix"))
+
+class VersionInfo(_VersionInfo):
+    def __str__(self):
+        res = ".".join( ( str(elt) for elt in self[:3] ) )
+        if self.releasesuffix:
+            res += self.releasesuffix
+        return res
+
+    def __repr__(self):
+        return "{0}.{1}".format(__name__, _VersionInfo.__repr__(self))
+
+version_info = VersionInfo(2, 13, 1, None)
+__version__ = str(version_info)
+
 
 import sys, os, tempfile, random, re, warnings, traceback
 from textwrap import dedent
