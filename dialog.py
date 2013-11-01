d444bbafc0f548290540c53e00d39007f61568c4
Florent Rougon <f.rougon@free.fr>
2013-11-01 16:26:19 +0100

    Release 3.0.1

--- dialog.py
+++ dialog.py
@@ -95,7 +95,7 @@ class VersionInfo(_VersionInfo):
     def __repr__(self):
         return "{0}.{1}".format(__name__, _VersionInfo.__repr__(self))
 
-version_info = VersionInfo(3, 0, 0, None)
+version_info = VersionInfo(3, 0, 1, None)
 __version__ = str(version_info)
 
 
