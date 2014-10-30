94cb93ef8251f021e654114d19065cacb16483e9
Florent Rougon <f.rougon@free.fr>
2014-10-30 10:08:32 +0100

    [Manual] Improve the tutorial
    * doc/intro/example.py: remove the useless "import dialog" statement at
    the beginning of the file
    
    * Expand on the example:
      - introduce the msgbox and infobox widgets as well;
      - present the 'title', 'backtitle', 'width' and 'height' common
        options;
      - show how to deal with the Dialog exit codes;
      - mention the autowidgetsize feature;
      - suggest good practices such as locale.setlocale(locale.LC_ALL, '')
        at the beginning of the program.

--- doc/intro/example.py
+++ doc/intro/example.py
@@ -26,12 +26,40 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import dialog
+import sys
+import locale
+import time
+
 from dialog import Dialog
+
+# This is almost always a good thing to do at the beginning of your programs.
+locale.setlocale(locale.LC_ALL, '')
 
 d = Dialog(dialog="dialog")
 
-code, tag = d.menu("Some text that will be displayed before the menu entries",
+button_names = {d.OK:     "OK",
+                d.CANCEL: "Cancel",
+                d.HELP:   "Help",
+                d.EXTRA:  "Extra"}
+
+code, tag = d.menu("Some text that will be displayed above the menu entries",
                    choices=[("Tag 1", "Item text 1"),
                             ("Tag 2", "Item text 2"),
                             ("Tag 3", "Item text 3")])
+
+if code == d.ESC:
+    d.msgbox("You got out of the menu by pressing the Escape key.")
+else:
+    text = "You got out of the menu by choosing the {} button".format(
+        button_names[code])
+
+    if code != d.CANCEL:
+        text += ", and the highlighted entry at that time had tag {!r}".format(
+        tag)
+
+    d.msgbox(text + ".", width=40, height=10)
+
+d.infobox("Bye bye...", width=0, height=0, title="This is the end")
+time.sleep(2)
+
+sys.exit(0)
