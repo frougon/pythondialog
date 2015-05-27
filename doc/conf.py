df3b3820ad54c6eedd59b9dfc675a9669f797a49
Florent Rougon <f.rougon@free.fr>
2015-05-27 22:42:24 +0200

    Use html_theme = 'classic' with Sphinx 1.3b3 or later
    * doc/conf.py: change the HTML theme name to 'classic' if the Sphinx
    version is 1.3b3 or later (the old name 'default' is now deprecated).

--- doc/conf.py
+++ doc/conf.py
@@ -108,7 +108,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+import sphinx
+if sphinx.version_info >= (1, 3, 0, 'beta', 3):
+    html_theme = 'classic'
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
